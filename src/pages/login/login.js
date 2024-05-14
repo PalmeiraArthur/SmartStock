97a348b7e03c80daed763fa52af0ff8ebf8b6ef2
Arthur Palmeira Omena Alencar Guimarães <135675360+PalmeiraArthur@users.noreply.github.com>
2024-05-14 18:06:53 -0300

    Mudança na estilização da tela de login
    Co-authored-by: Arthur Francisco <Hanyakawa@users.noreply.github.com>

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -19,26 +19,28 @@ function LoginPage() {
     };
 
     return (
-        <div className='flex justify-center'>
-            <img src='./img/logoPngAzul.png'/>
-            <div className=' absolute justify-center left-[850px] bottom-[300px] z-10 flex flex-col gap-[20px]  '>
-                <h1 className='text-white text-[30px] text-center font-semibold'>Acessar <br></br>estabelecimento</h1>
-                <form className='flex flex-col gap-[20px]' onSubmit={handleLogin}>
+        <div className='flex flex-col justify-center items-center'>
+
+            <img className='w-[330px] h-[330px]' src='./img/logoPngAzulSvg.svg'/>
+
+            <div className='justify-center items-center flex flex-col gap-[50px] pt-[40px] p-[80px] rounded-lg bg-verde-smartStock '>
+
+            <h1 className='text-white text-[30px] text-center font-semibold'>Acessar <br></br>estabelecimento</h1>
+                <form className='flex flex-col gap-[50px] items-center' onSubmit={handleLogin}>
+                    <div className='flex flex-col gap-[13px]'>
                     <label>
-                        <input className='bg-transparent border-[2px] p-[8px] rounded-md border-green-900 placeholder-green-900 placeholder:font-semibold placeholder:text-[20px] text-[20px] text-white' placeholder='Usuario:' type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                        <input className='bg-transparent border-[2px] p-[8px] rounded-md border-white placeholder-white placeholder:opacity-50 placeholder:font-semibold placeholder:text-[20px] text-[20px] text-white focus:placeholder:opacity-0 focus:outline-none ' placeholder='Usuario:' type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
                     </label>
 
                     <label>
-                        <input className='bg-transparent border-[2px] p-[8px] rounded-md border-green-900 placeholder-green-900 placeholder:font-semibold placeholder:text-[20px] text-[20px] text-white' placeholder='Senha:' type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <input className='bg-transparent border-[2px] p-[8px] rounded-md border-white placeholder-white placeholder:opacity-50 placeholder:font-semibold placeholder:text-[20px] text-[20px] text-white focus:placeholder:opacity-0 focus:outline-none '  placeholder='Senha:' type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                     </label>
+
+                    </div>
         
                     <button className='bg-green-900 text-white rounded-md w-[250px] h-[50px] text-[22px] font-bold' type="submit">Entrar</button>
                 </form>
             </div>
-        <div className='relative'> 
-            <img className='fixed left-0 bottom-[-50px] w-screen z-0' src='./img/fundoLogin.svg' />
-        </div>
-         
             
         </div>
     );
